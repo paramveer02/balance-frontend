0bfd18db7fab15182beaf0f82f675778d8c0be93
andruil <nautzernameistvergeben@gmail.com>
2025-08-26 20:33:24 +0200

    work on contextprovider

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -5,12 +5,14 @@ const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [isAuth, setIsAuth] = useState(false);
 
   useEffect(() => {
     const getUser = async () => {
       try {
         const userData = await me();
         setUser(userData);
+        setIsAuth(true);
       } catch (error) {
         console.log('Invalid Token');
       }
@@ -22,6 +24,8 @@ const AuthContextProvider = ({ children }) => {
   const values = {
     user,
     setUser,
+    isAuth,
+    setIsAuth,
   };
 
   return <AuthContext value={values}>{children}</AuthContext>; //to do value instead of 'values'
