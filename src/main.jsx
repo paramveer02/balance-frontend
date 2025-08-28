6857f956779dfe9055a9f77eddef4aa454dd8ecd
Paramveer <paramveermarwahafc@gmail.com>
2025-08-28 15:11:19 +0200

    Feat: toastify wrapper added

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
-import { AuthContextProvider } from './context/AuthContext.jsx';
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { StrictMode } from "react";
+import { ToastContainer } from "react-toastify";
 
-import './index.css';
-import App from './App.jsx';
-
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+    <ToastContainer position="top-center" />
+  </StrictMode>
 );
