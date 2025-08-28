923786ea50d7d9610961d986384ca4a58810ccf7
Paramveer <paramveermarwahafc@gmail.com>
2025-08-28 15:14:19 +0200

    feat: dashboard set as protected route; shows navbar and footer and globally shares logged in user with all its children

--- src/pages/application/Dashboard.jsx
+++ src/pages/application/Dashboard.jsx
@@ -1,5 +1,31 @@
+import { Outlet, redirect, useLoaderData } from "react-router-dom";
+import customFetch from "../../utils/customFetch";
+import DashboardContextProvider from "../../context/DashboardContext";
+import { Footer, Navbar } from "../../components";
+
+export const dashboardLoader = async () => {
+  try {
+    const { data } = await customFetch.get("/users/current-user");
+
+    return { user: data.user };
+  } catch (error) {
+    return redirect("/login");
+  }
+};
+
 const Dashboard = () => {
-  return <div>You're on Dashboard</div>;
+  const { user } = useLoaderData();
+
+  return (
+    <DashboardContextProvider initialUser={user}>
+      <main>
+        <Navbar />
+        <h2>Welcome to Dashboard, {user.name}</h2>
+        <Outlet context={{ user }} />
+        <Footer />
+      </main>
+    </DashboardContextProvider>
+  );
 };
 
 export default Dashboard;
