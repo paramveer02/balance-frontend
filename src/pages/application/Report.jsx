d68452d57dbc10581fd7e7640eb8bdb20b004c9d
Paramveer <paramveermarwahafc@gmail.com>
2025-08-28 15:14:33 +0200

    in progress: testing logged in user

--- src/pages/application/Report.jsx
+++ src/pages/application/Report.jsx
@@ -1,5 +1,8 @@
+import { useOutletContext } from "react-router-dom";
+
 const Report = () => {
-  return <div>You're on report page</div>;
+  const { user } = useOutletContext();
+  return <div>You're on report page, {user?.name}</div>;
 };
 
 export default Report;
